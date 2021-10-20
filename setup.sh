a9c0852f42d78db88d3a07bcac17b845424ba375
glamourousgecko <glamourousgecko@protonmail.com>
2021-10-20 15:35:58 -0400

    Add setup for script for installing and configuring profile for new firefox install

---
+++ setup.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+sudo apt install firejail
+firejail firefox -no-remote -ProfileManager
+cd ~/.mozilla/firefox/*.Hardened && rm -r * && wget https://github.com/maxthemage/Hardened-Firefox/blob/master/user.js
+cd /usr/lib/firefox/browser/features/
+rm -rf firefox@getpocket.com.xpi followonsearch@mozilla.com.xpi activity-stream@mozilla.org.xpi screenshots@mozilla.org.xpi onboarding@mozilla.org.xpi formautofill@mozilla webcompat@mozilla.org.xpi
+
