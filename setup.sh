a0ffb6ba7b526eb167f0ff9b09867c5b5edfe9ec
glamourousgecko <glamourousgecko@protonmail.com>
2021-11-28 14:35:55 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-sudo apt install firejail
-firejail firefox -no-remote -ProfileManager
+firefox -no-remote -ProfileManager
 cd ~/.mozilla/firefox/*.Hardened && rm -r * && wget https://raw.githubusercontent.com/maxthemage/Hardened-Firefox/master/user.js 
 cd /usr/lib/firefox/browser/features/
 rm -rf firefox@getpocket.com.xpi followonsearch@mozilla.com.xpi activity-stream@mozilla.org.xpi screenshots@mozilla.org.xpi onboarding@mozilla.org.xpi formautofill@mozilla webcompat@mozilla.org.xpi
