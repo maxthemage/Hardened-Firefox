1e3c7067d15be48a79d24653f07a40c008f17412
glamourousgecko <glamourousgecko@protonmail.com>
2021-11-28 14:33:06 -0400

    Modify setup.sh to include correct raw github content link

--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@
 
 sudo apt install firejail
 firejail firefox -no-remote -ProfileManager
-cd ~/.mozilla/firefox/*.Hardened && rm -r * && wget https://github.com/maxthemage/Hardened-Firefox/blob/master/user.js
+cd ~/.mozilla/firefox/*.Hardened && rm -r * && wget https://raw.githubusercontent.com/maxthemage/Hardened-Firefox/master/user.js 
 cd /usr/lib/firefox/browser/features/
 rm -rf firefox@getpocket.com.xpi followonsearch@mozilla.com.xpi activity-stream@mozilla.org.xpi screenshots@mozilla.org.xpi onboarding@mozilla.org.xpi formautofill@mozilla webcompat@mozilla.org.xpi
 
